2ef7aa86072563051e697f4ad408b4fef049d38e
Romina Fernandez F <romi.ferfu@gmail.com>
2024-07-01 13:12:13 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
 import { AppComponent } from './app.component';
 
 
@@ -17,6 +20,7 @@ import { AppComponent } from './app.component';
     CounterModule,
     AppRoutingModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
